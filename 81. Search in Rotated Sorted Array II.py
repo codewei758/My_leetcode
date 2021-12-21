7dc8ce1db10033c34bf8b69914512ca68d76cc0d
codev <zhangxiaowei758@gmail.com>
2021-12-21 13:17:11 +0800

    Commits

---
+++ 81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            x = nums[m]
+            if x == target:
+                return True
+            if x == nums[l]:
+                l += 1  # 去重重复值情况
+            elif nums[l] < x:  # 前半部分有序
+                if nums[l] <= target <= x:  # 目标值在前半部分
+                    r = m - 1
+                else:  # 反之，去后半部分找
+                    l = m + 1
+            else:  # 后半部分有序
+                if x <= target <= nums[r]:  # 目标值在后半部分
+                    l = m + 1
+                else:  # 反之，去前半部分找
+                    r = m - 1
+        return False  # 没找到值
